694148c0ddd5b9ce772794aa33c2347691f6d9cb
Your Name <you@example.com>
2020-10-25 16:55:55 +0530

    store

---
+++ storehome.js
@@ -0,0 +1,25 @@
+  function goto(){
+  var a=document.getElementById("search").value;
+
+  if (a=="among us") {
+    window.location.href=("https://m26gaming.github.io/among-us/");
+  }
+  if (a=="amazing spider man 2") {
+    window.location.href=("")
+  }
+  if (a=="human fall flat") {
+  	window.location.href=("https://m26gaming.github.io/redirect/")
+  }
+  if (a=="grand theft auto 5") {
+    window.location.href=("https://m26gaming.github.io/gta-5/")
+  }
+  if (a=="grand theft auto san andreas") {
+    window.location.href=("https://m26gaming.github.io/gtaSAN-redirect/")
+  }
+  if (a=="grand theft auto vice city") {
+    window.location.href=("https://m26gaming.github.io/gta-5/")
+  }
+  if (a=="grand theft auto 4") {
+    window.location.href=("https://m26gaming.github.io/gta-5/")
+  }
+}
